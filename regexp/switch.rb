1d601583da2edf4708a026d0f1594eb58d1a5fd9
Rob Wilson <roobert@gmail.com>
2013-12-22 15:54:38 +0000

    add translations

--- regexp/switch.rb
+++ regexp/switch.rb
@@ -1,36 +1,160 @@
 #!/usr/bin/env ruby
 
-module Switch
-  module Bridge
+class Switch
+  class Bridge
     class Table
-      def initialize(config)
-        @config = config
+      def initialize(table)
+        @table = table
         @interfaces = {}
         self.objectify
       end
 
       def objectify
-        @config.each do |name, interface|
-          @interfaces[name] = Switch::Bridge::Interface.new(name, interface)
+        @table.each do |identifier, entry|
+          @interfaces[identifier] = Switch::Bridge::Entry.new(identifier, entry)
         end
       end
     end
 
-    class Interface
+    class Entry
       attr_reader :name, :port, :unit, :switch_member, :vlans
 
-      def initialize(name, interface)
-        @name          = name
-        @port          = interface[:port]
-        @unit          = interface[:unit]
-        @switch_member = interface[:stack_member]
-        @vlans         = interface[:vlan]
+      def initialize(identifier, entry)
+        @identifier    = identifier 
+        @port          = entry[:port]
+        @unit          = entry[:unit]
+        @switch_member = entry[:stack_member]
+        @vlans         = entry[:vlan]
       end
     end
   end
 
-  module Interface
+  class Config
+    def initialize(config)
+      @config                  = config
+      @ethernet_interfaces     = {}
+      @vlan_interfaces         = {}
+      @port_channel_interfaces = {}
+      self.objectify
+    end
+
+    def objectify
+      @config[:interface].each do |type, interfaces|
+
+        case type
+
+        when :ethernet
+          interfaces.each do |identifier, interface|
+            @ethernet_interfaces[identifier] = Switch::Interface::Ethernet.new(identifier, interface)
+          end
+
+        when :vlan
+          interfaces.each do |identifier, interface|
+            @vlan_interfaces[identifier] = Switch::Interface::Vlan.new(identifier, interface)
+          end
+
+        when :port_channel
+          interfaces.each do |identifier, interface|
+            @port_channel_interfaces[identifier] = Switch::Interface::PortChannel.new(identifier, interface)
+          end
+
+        else
+          raise StandardError, "unknown interface type: #{type}"
+        end
+      end
+    end
+
+    def get_ethernet_interfaces
+      @ethernet_interfaces
+    end
+
+    def get_vlan_interfaces
+      @vlan_interfaces
+    end
+
+    def get_port_channel_interfaces
+      @port_channel_interfaces
+    end
+  end
+
+  class Interface
     class Ethernet
+      def initialize(identifier, interface)
+        @identifier   = identifier
+        @interface    = interface
+        @description  = interface[:description]
+        @stack_member = interface[:stack_member]
+        @port         = interface[:port]
+        @unit         = interface[:unit]
+        @switchport   = Switch::Interface::Switchport.new(interface[:switchport]) unless interface[:switchport].nil?
+      end
+
+      def inspect
+        "stack_member: #{@stack_member}, port: #{@port}, unit: #{@unit}, description: #{@description}, switchport: #{@switchport.inspect}".chomp
+      end
+
+      def mode
+        "mode: #{@mode} " unless @mode.nil?
+      end
+    end
+
+    class Vlan
+      def initialize(identifier, interface)
+        @identifier  = identifier
+        @interface   = interface
+        @vlan        = interface[:vlan]
+        @description = interface[:description]
+      end
+
+      def inspect
+        "vlan: #{@vlan}, description: #{@description}"
+      end
+    end
+
+    class PortChannel
+      def initialize(identifier, interface)
+        @identifier  = identifier
+        @interface   = interface
+        @description = interface[:description]
+        @channel     = interface[:channel]
+        @switchport  = Switch::Interface::Switchport.new(interface[:switchport])
+      end
+
+      def inspect
+        "channel: #{@channel}, description: #{@description}"
+      end
+    end
+
+    class Switchport
+      def initialize(switchport)
+        @switchport = switchport
+        @mode       = switchport[:mode]
+        @vlans      = Switch::Interface::Vlans.new(switchport[:vlans])
+      end
+
+      def inspect
+        "[mode: #{@mode}, vlans: #{@vlans.inspect}]"
+      end
+    end
+
+    class Vlans
+      def initialize(vlans)
+        @vlans  = vlans
+        @add    = vlans[:add]
+        @remove = vlans[:remove]
+      end
+
+      def inspect
+        [self.add, self.remove].join(', ').chomp.chomp
+      end
+
+      def add
+        "add: #{@add}" unless @add.nil?
+      end
+
+      def remove
+        "remove: #{@remove}" unless @remove.nil?
+      end
     end
   end
 end
--- regexp/test_parser.rb
+++ regexp/test_parser.rb
@@ -7,19 +7,29 @@ require_relative 'switch'
 require 'awesome_print'
 require 'hash_deep_merge'
 
-#config_parser = SwitchConfigParser.new(IO.readlines("../data/switch.config"))
-#config_parser.parse_config
-#ap config_parser.get_config
+config_parser = SwitchConfigParser.new(IO.readlines("../data/switch.config"))
+config_parser.parse_config
+switch_config = config_parser.get_config
 
 bridge_table_parser = SwitchBridgeTableParser.new(IO.readlines("../data/bridge_table.config"))
 bridge_table_parser.parse_config
-bridge_config = bridge_table_parser.get_config
+bridge_table = bridge_table_parser.get_config
 
 #ap config_parser.get_config[:interface][:ethernet].find_all { |name, interface|
-  #interface[:port] == "5" and interface[:unit] == 'g' and interface[:stack_member] == "1"
+#  interface[:port] == "5" and interface[:unit] == 'g' and interface[:stack_member] == "1"
 #}
+
+#ap switch_config[:interface][:port_channel]
+#ap bridge_table
 
 #ap config_parser.get_config.deep_merge(bridge_table_parser.get_config)
 
-ap Switch::Bridge::Table.new(bridge_config)
+t = Switch::Bridge::Table.new(bridge_table)
+c = Switch::Config.new(switch_config)
+
+#ap t
+ap c.get_vlan_interfaces
+ap c.get_port_channel_interfaces
+ap c.get_ethernet_interfaces
+
 
