15f2d58eaa67e4a3c26ece819e8128f0ce94c2b0
Rob Wilson <roobert@gmail.com>
2013-12-22 10:43:30 +0000

    updated parser

--- regexp/parse_config.rb
+++ regexp/parse_config.rb
@@ -7,6 +7,14 @@ class SwitchConfigParser
     @config = {}
   end
 
+  def setup_hash(type, line)
+    @config[:interface] ||= {}
+    @config[:interface][type] ||= {}
+    identifier = line.gsub(/ /, '_').gsub(/-/, '_').gsub(/\//, '_').chomp
+    @config[:interface][type][identifier] ||= {}
+    @config[:interface][type][identifier]
+  end
+
   def parse_config
 
     @io.each do |line|
@@ -20,32 +28,22 @@ class SwitchConfigParser
         next
 
       when /interface ethernet/ then
-
-        @config[:interface] ||= {}
-        @config[:interface][:ethernet] ||= {}
-
+        @interface_ethernet = setup_hash(:ethernet, line)
+        @in_interface_block = :ethernet
         parse_interface_ethernet(line)
-        @in_interface_block = 'ethernet'
         next
 
       when /interface vlan/ then
-
-        @config[:interface] ||= {}
-        @config[:interface][:vlan] ||= {}
-
-        @in_interface_block = 'vlan'
+        @interface_vlan = setup_hash(:vlan, line)
+        @in_interface_block = :vlan
+        parse_interface_vlan(line)
         next
 
       when /interface port-channel/ then
-
-        @config[:interface] ||= {}
-        @config[:interface][:port_channel] ||= {}
-        @identifier = line.gsub(/ /, '_').gsub(/-/, '_').chomp
-        @config[:interface][:port_channel][@identifier] ||= {}
-
-        @in_interface_block = 'port_channel'
+        @interface_port_channel = setup_hash(:port_channel, line)
+        @in_interface_block = :port_channel
+        parse_interface_port_channel(line)
         next
-
 
       when /^exit/ then
         @in_interface_block = false
@@ -54,9 +52,9 @@ class SwitchConfigParser
       end
 
       case @in_interface_block
-      when 'ethernet'     then parse_interface_ethernet(line)
-      when 'vlan'         then parse_interface_vlan(line)
-      when 'port_channel' then parse_interface_port_channel(line)
+      when :ethernet     then parse_interface_ethernet(line)
+      when :vlan         then parse_interface_vlan(line)
+      when :port_channel then parse_interface_port_channel(line)
       else
         puts "unrecognised line: #{line}" if @debug
       end
@@ -70,13 +68,18 @@ class SwitchConfigParser
   private
 
   def parse_vlan_line(line)
-    ranges = line.split(' ')[5].split(',')
+    if line =~ /switchport access vlan/
+      ranges = line.split(' ')[3].split(',')
+    else
+      ranges = line.split(' ')[5].split(',')
+    end
 
     vlans = ranges.map do |range|
       if range =~ /-/
         a = range.split('-')
         range = (a[0]..a[1]).to_a
       end
+      range
     end
 
     vlan_hash = {}
@@ -86,18 +89,54 @@ class SwitchConfigParser
     vlan_hash
   end
 
+  def parse_switchport(line, switchport)
+    case line
+    when /switchport access vlan/
+      switchport[:mode] = 'access'
+      switchport[:vlans] ||= {}
+      switchport[:vlans][:add] = parse_vlan_line(line)
+
+    when /switchport mode trunk/
+      switchport[:mode] ||= {}
+      switchport[:mode] = 'trunk'
+
+    when /switchport mode general/
+      switchport[:mode] ||= {}
+      switchport[:mode] = 'general'
+
+    when /switchport trunk allowed vlan add/
+      switchport[:vlans] ||= {}
+      switchport[:vlans][:add] = parse_vlan_line(line)
+
+    when /switchport trunk allowed vlan remove/
+      switchport[:vlans] ||= {}
+      switchport[:vlans][:remove] = parse_vlan_line(line)
+
+    when /switchport general allowed vlan add/
+      switchport[:vlans] ||= {}
+      switchport[:vlans][:add] = parse_vlan_line(line)
+
+    when /switchport general allowed vlan remove/
+      switchport[:vlans] ||= {}
+      switchport[:vlans][:remove] = parse_vlan_line(line)
+
+    when /switchport general acceptable-frame-type/
+      switchport[:acceptable_frame_type] = line.split[-1]
+
+    else
+      puts "unrecognised line: #{line}" if @debug
+    end
+  end
+
   def parse_interface_vlan(line)
     case line
+
     when /interface vlan/
-      vlan = line.split[2]
-      @identifier = line.gsub(' ', '_').gsub('/', '_').chomp
-      @config[:interface][:vlan][@identifier] ||= {}
-      @config[:interface][:vlan][@identifier].merge!({ :vlan => vlan })
+      @interface_vlan.merge!({ :vlan => line.split[2] })
+
     when /name/
-      vlan = line.split[2]
-      @identifier = line.gsub(' ', '_').gsub('/', '_').chomp
-      @config[:interface][:vlan][@identifier] ||= {}
-      @config[:interface][:vlan][@identifier][:description] = line.gsub(/name "/, '').chomp.chomp('"')
+      @interface_vlan[:description] = line.gsub(/name "/, '').chomp.chomp('"')
+
     else
       puts "unrecognised line: #{line}" if @debug
     end
@@ -105,17 +144,11 @@ class SwitchConfigParser
 
   def parse_interface_ethernet(line)
     case line
+
     when /interface ethernet/
-
-      @identifier = line.gsub(' ', '_').gsub('/', '_').chomp
-
-      @interface_ethernet = @config[:interface][:ethernet][@identifier]
-
-      @interface_ethernet = {
-        :unit => line.split[2].split('/')[0],
-        :port => line.split[2].split('/')[1].gsub(/[a-z]*/, ''),
-        :type => line.split[2].split('/')[1].gsub(/[0-9]*/, ''),
-      }
+      @interface_ethernet[:unit] = line.split[2].split('/')[0]
+      @interface_ethernet[:unit] = line.split[2].split('/')[1].gsub(/[a-z]*/, '')
+      @interface_ethernet[:unit] = line.split[2].split('/')[1].gsub(/[0-9]*/, '')
 
     when /description/
       @interface_ethernet[:description] = line.gsub(/description '/, '').chomp.chomp('\'')
@@ -127,42 +160,8 @@ class SwitchConfigParser
       }
 
     when /switchport/
-      @interface_ethernet[:switchport] ||= {}
-      @interface_ethernet[:switchport][:mode] ||= {}
-      @interface_ethernet[:switchport][:vlans] ||= {}
+      parse_switchport(line, @interface_ethernet[:switchport] ||= {})
 
-      switchport = @interface_ethernet[:switchport]
-
-      case line
-      when /switchport access/
-        switchport[:mode] = 'access'
-
-      when /switchport mode trunk/
-        switchport[:mode] = 'trunk'
-
-      when /switchport mode general/
-        switchport[:mode] = 'general'
-
-      when /switchport access vlan/
-        switchport[:vlans][:add] = parse_vlan_line(line)
-
-      when /switchport trunk allowed vlan add/
-        switchport[:vlans][:add] = parse_vlan_line(line)
-
-      when /switchport trunk allowed vlan remove/
-        switchport[:vlans][:remove] = parse_vlan_line(line)
-
-      when /switchport general allowed vlan add/
-        switchport[:vlans][:add] = parse_vlan_line(line)
-
-      when /switchport general allowed vlan remove/
-        switchport[:vlans][:remove] = parse_vlan_line(line)
-
-      when /switchport general acceptable-frame-type/
-        switchport[:acceptable_frame_type] = line.split[-1]
-      else
-        puts "unrecognised line: #{line}" if @debug
-      end
     else
       puts "unrecognised line: #{line}" if @debug
     end
@@ -170,11 +169,16 @@ class SwitchConfigParser
 
   def parse_interface_port_channel(line)
     case line
+
     when /interface port-channel/
-      channel = line.split[2]
-      @config[:interface][:port_channel][@identifier] = { :channel => channel }
+      @interface_port_channel.merge!({ :channel => line.split[2] })
+
     when /description/
-      @config[:interface][:port_channel][@identifier][:description] = line.gsub(/description '/, '').chomp.chomp('\'')
+      @interface_port_channel[:description] = line.gsub(/description '/, '').chomp.chomp('\'')
+
+    when /switchport/
+      parse_switchport(line, @interface_port_channel[:switchport] ||= {})
+
     else
       puts "unrecognised line: #{line}" if @debug
     end
--- regexp/test_parser.rb
+++ regexp/test_parser.rb
@@ -8,10 +8,10 @@ require 'hash_deep_merge'
 
 config_parser = SwitchConfigParser.new(IO.readlines("../data/switch.config"))
 config_parser.parse_config
-#ap config_parser.get_interfaces
+ap config_parser.get_config
 
-bridge_table_parser = SwitchBridgeTableParser.new(IO.readlines("../data/bridge_table.config"))
-bridge_table_parser.parse_config
-#ap bridge_table_parser.get_interfaces
+#bridge_table_parser = SwitchBridgeTableParser.new(IO.readlines("../data/bridge_table.config"))
+#bridge_table_parser.parse_config
+#ap bridge_table_parser.get_config
 
-ap config_parser.get_config.deep_merge(bridge_table_parser.get_config)
+#ap config_parser.get_config.deep_merge(bridge_table_parser.get_config)
